00d516728b777a072162848d51fafaaa79f85259
Yasith_99 <yasithsarada79@gmail.com>
2025-07-07 17:57:26 +0530

    first commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtomicSteps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] { "Alice", "Bob", "Charlie", "Diana" };
+            return Ok(students);
+        }
+    }
+}
