0e6f98fb2ffc8bc3a3a1970f6e60445097956ff5
Yasith_99 <yasithsarada79@gmail.com>
2025-08-13 00:39:51 +0530

    08/13 atomic way to .net expertise

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtomicSteps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllAnimals()
+        {
+            string[] students = new string[] { "Alice", "Bob", "Charlie", "Diana" };
+            return Ok(students);
+
+        }
+    }
+}
---
+++ Models/Domain/Region.cs
@@ -0,0 +1,6 @@
+﻿namespace AtomicSteps.Models.Domain
+{
+    public class Region
+    {
+    }
+}
